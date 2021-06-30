1531a64e72187846b7118ef516832f43cb7b9d6b
Jordi Sabaté <jordi@webcoop.cat>
2021-06-30 10:13:37 +0200

    server url config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', ''),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'dbbad98142ac7655850929768dc95340'),
