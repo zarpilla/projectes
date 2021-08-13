dec91360f87277c09ded9fb773d8a1304beaec50
Jordi <zarpa37@gmail.com>
2021-08-13 12:08:52 +0200

    month after cycle for sorting

--- api/month/models/month.js
+++ api/month/models/month.js
@@ -1,8 +1,28 @@
 'use strict';
 
+const _ = require('lodash');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        afterFind: async (results, params, populate) => {
+            if (params && !params._sort) {
+                const sorted = _.sortBy(results, 'month')
+                const sortedbk = JSON.parse(JSON.stringify(sorted))
+                // mitate, not reassign
+                results.forEach((res, i) => {
+                    res.id = sortedbk[i].id
+                    res.month = sortedbk[i].month                    
+                    res.month_number = sortedbk[i].month_number
+                    res.name = sortedbk[i].name
+                })
+            }
+        },
+    }
+};
+
+
