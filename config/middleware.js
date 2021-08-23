224c1b7668614696bf06d3b5cedf0141ffbe4f5e
Jordi <zarpa37@gmail.com>
2021-08-23 12:08:51 +0200

    disable brotli

---
+++ config/middleware.js
@@ -0,0 +1,13 @@
+module.exports = {
+    settings: {
+    //   cors: {
+    //     origin: ['http://localhost:3000', 'https://name.tld', 'https://api.name.tld'],
+    //   },
+      gzip: {
+        enabled: true,
+        options: {
+          br: false
+        }
+      }
+    },
+  };
