3158dfce8b4bfdf2bce6466bd060f0a596a9ab74
Jordi <zarpa37@gmail.com>
2022-01-31 09:50:10 +0100

    dedication overlapping controñ

--- api/daily-dedication/models/daily-dedication.js
+++ api/daily-dedication/models/daily-dedication.js
@@ -5,4 +5,23 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            const dedications = await strapi.query('daily-dedication').find({ users_permissions_user: data.users_permissions_user, _limit: -1 });            
+            const valids = dedications
+            const invalids = valids.filter(d => (data.to >= d.from && data.to <= d.to) || (data.from <= d.to && data.to >= d.from))
+            if (invalids.length) {
+                throw new Error('daily-dedication overlaps')
+            }
+        },
+        async beforeUpdate(params, data) {            
+            const dedications = await strapi.query('daily-dedication').find({ users_permissions_user: data.users_permissions_user, _limit: -1 });            
+            const valids = dedications.filter(d => d.id !== params.id)
+            const invalids = valids.filter(d => (data.to >= d.from && data.to <= d.to) || (data.from <= d.to && data.to >= d.from))
+            if (invalids.length) {
+                throw new Error('daily-dedication overlaps')
+            }
+        }
+      },
+};
