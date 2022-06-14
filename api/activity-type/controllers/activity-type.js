154c91e370b5ac9c7027315d69c91a2d01661013
Jordi <zarpa37@gmail.com>
2022-06-14 22:18:54 +0200

    multiple activity type

--- api/activity-type/controllers/activity-type.js
+++ api/activity-type/controllers/activity-type.js
@@ -5,4 +5,18 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async updateGlobal(ctx) {
+        const acTypes = await strapi.query("activity-type").find({ _limit: -1 });
+
+        for(let i = 0; i < acTypes.length; i++) {
+            if (acTypes[i].project && acTypes[i].project.id) {
+
+                const acTypeToUpdate = { id: acTypes[i].id, projects: [acTypes[i].project.id] }
+
+                await strapi.query("activity-type").update({ id: acTypes[i].id }, acTypeToUpdate); 
+            }
+        }
+        return acTypes
+    }
+};
