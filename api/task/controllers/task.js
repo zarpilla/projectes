8c825607b30fa79bf3c41b94fa20b217592b0a6a
Jordi <zarpa37@gmail.com>
2022-06-23 15:52:08 +0200

    email to users involved in tasks

--- api/task/controllers/task.js
+++ api/task/controllers/task.js
@@ -9,19 +9,33 @@ let sendEmail = async (data, config) => {
 
   let emailFrom = strapi.config.get("plugins.email.settings.defaultFrom", "");
 
-  let text = `Atenció, tens <a target='_blank' href='${config.front_url}tasks'>tasques o checklists</a> amb la data límit superada:<br><br>`;
-  data.rows.forEach((r) => {
-    text += ` - ${r.name}: ${moment(r.date, "YYYY-MM-DD").format(
-      "DD/MM/YYYY"
-    )}<br>`; // " - " + r.name +  "<br>";
-  });
+  let text = ''
+  if (data.rows.filter(r => r.type === 'expired').length > 0) {
+    text += `Hi ha <a target='_blank' href='${config.front_url}tasks'>tasques o checklists</a> amb la data límit superada:<br><br>`;
+    data.rows.filter(r => r.type === 'expired').forEach((r) => {
+      text += ` - ${r.name}: ${moment(r.date, "YYYY-MM-DD").format(
+        "DD/MM/YYYY"
+      )}<br>`; // " - " + r.name +  "<br>";
+    });
+    text += '<br><br>'
+  }
+
+
+  if (data.rows.filter(r => r.type === 'new').length > 0) {
+    text += `Hi ha canvis en <a target='_blank' href='${config.front_url}tasks'>tasques o checklists</a>:<br><br>`;
+    data.rows.filter(r => r.type === 'new').forEach((r) => {
+      text += ` - ${r.name}<br>`; // " - " + r.name +  "<br>";
+    });
+    text += '<br><br>'
+  }
+  
 
   // send an email by using the email plugin
   if (data.email === process.env.TASK_EMAIL_TO || process.env.TASK_EMAIL_TO === '*') {
     await strapi.plugins["email"].services.email.send({
       to: data.email,
       from: emailFrom,
-      subject: "[STRAPI COOP] Tasques amb la data límit superada",
+      subject: "[STRAPI COOP] Resum de tasques",
       html: text,
     });
   }
@@ -48,6 +62,7 @@ module.exports = {
           project: e.project?.name,
           username: u.username,
           email: u.email,
+          type: "expired",
           scope: "task",
           date: e.due_date,
         });
@@ -74,6 +89,7 @@ module.exports = {
           project: e.project?.name,
           username: u.username,
           email: u.email,
+          type: "expired",
           scope: "task-checklist",
           date: e.due_date,
         });
@@ -85,6 +101,7 @@ module.exports = {
               project: e.project?.name,
               username: c.user?.username,
               email: c.user?.email,
+              type: "expired",
               scope: "checklist",
               date: c.due_date,
             });
@@ -92,6 +109,61 @@ module.exports = {
         });
       });
     });
+
+    const onedayBefore = moment().add(-1, 'days')
+    const newTasks = tasks.filter(
+      (t) => moment(t.updated_at).isAfter(onedayBefore)
+    );
+
+    newTasks.forEach((e) => {
+      e.users_permissions_users.forEach((u) => {
+        if (e.created && e.created.username && u.username !== e.created.username) {
+          messages.push({
+            id: e.id,
+            name: e.name,
+            project: e.project?.name,
+            username: u.username,
+            email: u.email,
+            type: "new",
+            scope: "task",
+            date: e.due_date,
+          });
+        }        
+      });
+    });
+
+    const newChecklists = tasks.filter(
+      (t) => t.checklist &&
+        t.checklist.length &&
+        t.checklist.find(
+        (c) =>
+          c.done === false &&
+          moment(c.created_date).isAfter(onedayBefore) &&
+          c.created && c.created.username && c.user && c.user.username && c.created.username !== c.user.username
+      )
+    );
+
+    newChecklists.forEach((e) => {
+
+      e.checklist.forEach((c) => {
+        if (c.done === false &&
+          moment(c.created_date).isAfter(onedayBefore) &&
+          c.created && c.created.username && c.user && c.user.username && c.created.username !== c.user.username) {
+          messages.push({
+            id: e.id,
+            name: e.name,
+            project: e.project?.name,
+            username: c.user?.username,
+            email: c.user?.email,
+            type: "new",
+            scope: "checklist",
+            date: c.due_date,
+          });
+        }
+      });
+            
+    });
+  
 
     const emails = _(messages)
       .groupBy("email")
@@ -114,6 +186,6 @@ module.exports = {
       }
     }
 
-    return { expired, checklists, messages, emails, sentEmails };
+    return { expired, checklists, messages, newTasks, newChecklists, emails, sentEmails };
   },
 };
