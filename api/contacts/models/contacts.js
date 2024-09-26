d4d9b67edc95092d367b8a8aa0cd855c638edfca
Jordi Sabaté <3697530+zarpilla@users.noreply.github.com>
2024-09-26 07:55:53 +0200

    contacts can be deleted

--- api/contacts/models/contacts.js
+++ api/contacts/models/contacts.js
@@ -1,8 +1,27 @@
 'use strict';
 
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeDelete(params) {
+            const id = params.id;
+            const projects = await strapi.query('project').findOne({ 'clients.id': id });
+            const intercooperations = await strapi.query('project').findOne({ 'intercooperations.id': id });
+            const orders = await strapi.query('orders').findOne({ 'contact.id': id });
+            const invoices = await strapi.query('emitted-invoice').findOne({ 'contact.id': id });
+            const incomes = await strapi.query('received-income').findOne({ 'contact.id': id });
+            const received = await strapi.query('received-invoice').findOne({ 'contact.id': id });
+            const expenses = await strapi.query('received-expense').findOne({ 'contact.id': id });
+            const leaders = await strapi.query('project').findOne({ 'grantable_leader.id': id });
+            const quotes = await strapi.query('quote').findOne({ 'contact.id': id });
+            if (projects || intercooperations || orders || invoices || incomes || received || expenses || leaders || quotes) {
+                throw new Error('You cannot delete this contact');
+            }
+          },
+        }
+};
